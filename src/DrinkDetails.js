f58b6663632c94077dcff367715c99d6f13467c3
Nathan Conn <nconn34@gmail.com>
2022-06-03 23:55:51 -0700

    Edited h2 tags and added return statements for prices

--- src/DrinkDetails.js
+++ src/DrinkDetails.js
@@ -3,10 +3,10 @@ import useFetch from "./useFetch";
 import React from "react";
 const DrinkDetails = () => {
     const {id} = useParams()
-    const {data: drink, error, isPending} = useFetch('http://localhost:8001/drinks/' + id);
+    const {data: drink, error, isPending} = useFetch('http://localhost:3000/drinks/' + id);
     const history = useHistory();
     const handleClick= () =>{
-        fetch('http://localhost:8001/drinks/' + drink.id, {
+        fetch('http://localhost:3000/drinks/' + drink.id, {
             method: 'DELETE'
         }).then(() =>{
             history.push('/')
@@ -19,12 +19,12 @@ const DrinkDetails = () => {
             {drink && (
                 <article>
                     <h2>{drink.name}</h2>
-                    <h2>{drink.type}</h2>
-                    <h2>{drink.brewer}</h2>
-                    <h2>{drink.abv}</h2>
-                    <h2>{drink.pint}</h2>
-                    <h2>{drink.growler}</h2>
-                    <h2>{drink.six}</h2>
+                    <h2>Type: {drink.type}</h2>
+                    <h2>Brewery: {drink.brewer}</h2>
+                    <h2>ABV (%) : {drink.abv}</h2>
+                    <h2>Price per pint: {drink.pint}</h2>
+                    <h2>Price per growler: {drink.growler}</h2>
+                    <h2>Price per six-pack: {drink.six}</h2>
                     <button onClick={handleClick}>Delete Drink</button>
                 </article>
             )}
