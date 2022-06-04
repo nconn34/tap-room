6cb4de8e14a08c0eb8f68fba0b2bdc51e16cb65d
Nathan Conn <nconn34@gmail.com>
2022-06-03 20:11:14 -0700

    Created function Sales and utilized Props

---
+++ src/Sales.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes  from "prop-types";
+
+function Sales(props){
+  return (
+    <React.Fragment>
+      {/* <h3>{props.name}</h3> */}
+      <p><em>{props.serving1}</em></p>
+      <p><em>{props.serving2}</em></p>
+      <p><em>{props.serving3}</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+Sales.propTypes = {
+    // name: PropTypes.string.isRequired,
+    serving1: PropTypes.string.isRequired,
+    serving2: PropTypes.string.isRequired,
+    serving3: PropTypes.string.isRequired,
+  };
+
+export default Sales;
