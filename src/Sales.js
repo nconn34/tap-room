d6f56bb3542c3aec59b70c6e4055b98c958e06e5
Nathan Conn <nconn34@gmail.com>
2022-06-03 23:57:48 -0700

    Updated prop types and fragments

--- src/Sales.js
+++ src/Sales.js
@@ -5,9 +5,10 @@ function Sales(props){
   return (
     <React.Fragment>
       {/* <h3>{props.name}</h3> */}
-      <p><em>{props.serving1}</em></p>
+      {/* <p><em>{props.serving1}</em></p>
       <p><em>{props.serving2}</em></p>
-      <p><em>{props.serving3}</em></p>
+      <p><em>{props.serving3}</em></p> */}
+      <p><em>{props.remaining}</em></p>
       <hr/>
     </React.Fragment>
   );
@@ -15,9 +16,10 @@ function Sales(props){
 
 Sales.propTypes = {
     // name: PropTypes.string.isRequired,
-    serving1: PropTypes.string.isRequired,
-    serving2: PropTypes.string.isRequired,
-    serving3: PropTypes.string.isRequired,
+    // serving1: PropTypes.string.isRequired,
+    // serving2: PropTypes.string.isRequired,
+    // serving3: PropTypes.string.isRequired,
+    remaining: PropTypes.string.isRequired
   };
 
 export default Sales;
