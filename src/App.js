aeee78b0c4201dbb4da8dd751f03f85814b531b8
Nathan Conn <nconn34@gmail.com>
2022-06-03 23:54:23 -0700

    Edited import statements and control routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './Create';
+import DrinkDetails from './DrinkDetails';
+import SalesControl from './SalesControl';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="content">
+        <Switch>
+          <Route exact path = "/">
+            <Home />
+          </Route>
+          <Route path = "/create">
+            <Create />
+          </Route>
+          <Route path = "/drinks/:id">
+            <DrinkDetails />
+            <SalesControl />
+          </Route>
+        </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
 
