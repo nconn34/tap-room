c315504c027ff656ee546e40b4129e008287b2c9
Nathan Conn <nconn34@gmail.com>
2022-06-05 11:10:23 -0700

    Attempted to branch logic for sales buttons but syntax is off

--- src/SalesControl.js
+++ src/SalesControl.js
@@ -5,16 +5,77 @@ class SalesControl extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          formVisibleOnPage: false
+          formVisibleOnPage: false,
+          ouncesRemaining: "1984",
+          selectedDrink: null,
+          editing: false
         };
       }
 
+      handleClick = () => {
+        if (this.state.selectedDrink != null) {
+          this.setState({
+            formVisibleOnPage: false,
+            selectedDrinks: null,
+            editing: false
+          });
+        } else {
+          this.setState(prevState => ({
+            formVisibleOnPage: !prevState.formVisibleOnPage,
+          }));
+        }
+      } 
+
+      handleSellingPint = () => {
+        const newMainSalesList = this.state.mainSalesList - "16";
+        this.setState({
+          mainSalesList: newMainSalesList,
+          selectedDrink: null
+        })
+      }
+      handleSellingGrowler = () => {
+        const newMainSalesList = this.state.mainSalesList - "64";
+        this.setState({
+          mainSalesList: newMainSalesList,
+          selectedDrink: null
+        })
+      }
+      handleSellingSixPack = () => {
+        const newMainSalesList = this.state.mainSalesList - "72";
+        this.setState({
+          mainSalesList: newMainSalesList,
+          selectedDrink: null
+        });
+      }
+
+
+
       render(){
         let currentlyVisibleState = null;
-          currentlyVisibleState = <SalesList />;
+        let buttonText = null;
+
+        // if (this.state.selectedDrink != null){
+        //   currentlyVisibleState = 
+        //   <SalesList
+        //   sales = {this.state.selectedDrink}
+        //   onClickingPint = {this.handleSellingPint}/>
+        //   buttonText = "Pint";
+        // }
+        // else if (this.state.selectedDrink !=null){
+        //   <SalesList
+        //   sales = {this.state.selectedDrink}
+        //   onClickingGrowler = {this.handleSellingGrowler}/>
+        //   buttonText = "Growler";
+        // }
+        // else if (this.state.selectedDrink !=null){
+        //   <SalesList
+        //   sales = {this.state.selectedDrink}
+        //   onClickingSixPack = {this.handleSellingSixPack}/>
+        //   buttonText = "Six-Pack";
         return (
           <React.Fragment>
             {currentlyVisibleState}
+            <button onClick={this.handleClick}>{buttonText}</button>
           </React.Fragment>
         );
       }
