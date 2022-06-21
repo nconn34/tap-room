2f267fead7a757b3fdbc471f060bf69b2d8dbbfe
Nathan Conn <nconn34@gmail.com>
2022-06-21 12:24:33 -0700

    Commented out code

--- src/Sales.js
+++ src/Sales.js
@@ -1,17 +1,17 @@
-import React from "react";
-import PropTypes  from "prop-types";
+// import React from "react";
+// import PropTypes  from "prop-types";
 
-function Sales(props){
-  return (
-    <React.Fragment>
-      <p><em>{props.remaining}</em></p>
-      <hr/>
-    </React.Fragment>
-  );
-}
+// function Sales(props){
+//   return (
+//     <React.Fragment>
+//       <p><em>{props.remaining}</em></p>
+//       <hr/>
+//     </React.Fragment>
+//   );
+// }
 
-Sales.propTypes = {
-    remaining: PropTypes.string.isRequired
-  };
+// Sales.propTypes = {
+//     remaining: PropTypes.string.isRequired
+//   };
 
-export default Sales;
+// export default Sales;
