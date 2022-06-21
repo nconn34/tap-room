2d9a8ef59bdc18181832dc1498535bb5ae8f6c2f
Nathan Conn <nconn34@gmail.com>
2022-06-21 12:30:49 -0700

    Added a keg tracker for each beverage added

--- src/DrinkDetails.js
+++ src/DrinkDetails.js
@@ -1,10 +1,13 @@
 import { useHistory, useParams } from "react-router-dom";
+import { useState } from "react";
 import useFetch from "./useFetch";
 import React from "react";
 const DrinkDetails = () => {
     const {id} = useParams()
     const {data: drink, error, isPending} = useFetch('http://localhost:3000/drinks/' + id);
     const history = useHistory();
+    const [count, setCount] = useState(1984);
+   
     const handleClick= () =>{
         fetch('http://localhost:3000/drinks/' + drink.id, {
             method: 'DELETE'
@@ -12,6 +15,13 @@ const DrinkDetails = () => {
             history.push('/')
         })
     }
+    const decrementPint = () => {
+        setCount(count - 16);
+    }
+    const decrementGrowler = () => {
+        setCount(count - 64);
+    }
+   
     return ( 
         <div className="location-details">
             {isPending && <div>Loading...</div>}
@@ -24,8 +34,10 @@ const DrinkDetails = () => {
                     <h2>ABV (%) : {drink.abv}</h2>
                     <h2>Price per pint: {drink.pint}</h2>
                     <h2>Price per growler: {drink.growler}</h2>
-                    <h2>Price per six-pack: {drink.six}</h2>
                     <button onClick={handleClick}>Delete Drink</button>
+                    {'\n'}<button onClick={decrementPint}>Sold a pint</button>
+                    {'\n'}<button onClick={decrementGrowler}>Sold a Growler</button>
+                    {count}
                 </article>
             )}
         </div>
